948e5365897ad1210bdf10667213f36148403235
Supun Induwara <induwarabas@gmail.com>
2020-04-14 11:32:51 +0530

    Changed formattings

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,17 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     ],
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": "off"
+    },
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
-    parserOptions: {ecmaVersion: 6},
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true // Allows for the parsing of JSX
+        }
+    },
 };
